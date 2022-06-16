859149a309c58607b7eade8dc673dd583600316a
Kathy Ems <kathy-ems@users.noreply.github.com>
2022-06-16 08:17:36 -0700

    show tasks with delete option

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="add task" color="green" />
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String
+        // title: {
+        //     type: String,
+        //     default: 'Default Header'
+        // }
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
